e87aa778be6b30ec06095068582a31ab65e978d3
Liska <07liska@gmail.com>
2024-06-08 12:15:29 -0400

    Added Cleaner

---
+++ Tests/API/ProjectCleaner.cs
@@ -0,0 +1,27 @@
+﻿using Allure.NUnit.Attributes;
+using TestRail.BaseEntities;
+
+
+namespace TestRail.Tests.API
+{
+    [Category("ProjectCleaner")]
+    public class ProjectCleaner : BaseTest
+    {
+        [Test]
+        [NonParallelizable] //Runs after all tests
+        [AllureDescription("Used to delete all my projects")]
+        public void DeleteAllMyProjects()
+        {
+            string projectName = "EAntonova";
+            userStep.SuccessfulLogin();
+
+            int quantityMyProjects = navigationStep.NavigateToProjectList().GetProgectListByPartialName(projectName).Count();
+            logger.Info($"Need to delete {quantityMyProjects} my projects");
+
+            for (int i = 0; i < quantityMyProjects; i++)
+            {
+                projectStep.DeleteProjectByName(projectName);
+            }
+        }
+    }
+}
